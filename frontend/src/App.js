9b44318bb52be9f404d057fd1725e195ec5103cf
Christer M.L. Bendixen <christer_github@fortelle.no>
2023-01-24 18:18:37 +0100

    removed qs, added axios, created useFetch.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
+import {createContext} from "react";
+import { fetchAPI } from '../../lib/api';
+import { getStrapiMedia } from '../../lib/media';
 
+export const GlobalContext = createContext({});
 
 function App() {
   return (
     <div className="App">
-      <h1>LETS GET CODING!</h1>
+      <h1>LETS GET CODINk!</h1>
     </div>
   );
 }
